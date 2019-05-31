7c7407cbac9d311d1d1388214171932d8a3464b1
luominyu <43443514+luominyu@users.noreply.github.com>
2019-05-31 22:58:20 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main() {
-    console.log("Debug Info");
+     var figures0 = ['._.\n' ,'|.|\n','|_|\n'];
+     var figures1 = ['...\n', '..|\n', '..|\n'];
+     var figures2 = ['._.\n', '._|\n','._|\n'];
+     var figures3 = ['._.\n', '._|\n','._|\n'];
+     var figures4 = ['...\n', '|_|\n','..|\n'];
+     var figures5 = ['._.\n', '|_.\n','._|\n'];
+     var figures6 = ['._.\n', '|_.\n','|_|\n'];
+     var figures7 = ['._.\n', '..|\n','..|\n'];
+     var figures8 = ['._.\n', '|_|\n','|_|\n'];
+     var figures9 = ['._.\n', '|_|\n','..|\n'];
+     console.log(figures9+figures8+figures0);
+     console.log("Debug Info");
     return 'Hello World!';
 };
